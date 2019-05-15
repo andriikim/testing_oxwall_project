2b55357582d42e544931a40dc9b3b64830f3fe9d
Olena Piankova <el.piankova@gmail.com>
2019-05-15 23:49:18 +0300

    Paramerize test by data from file

--- test_status_create.py
+++ test_status_create.py
@@ -1,8 +1,14 @@
+import json
+import pytest
+
 from pages.pages import DashboardPage
 
 
-def test_create_status(driver, logged_user):
-    input_text = 'Hello, world!!'
+with open("data/status_data.json", encoding="utf8") as f:
+    status_text_list = json.load(f)
+
+@pytest.mark.parametrize("input_text", status_text_list)
+def test_create_status(driver, logged_user, input_text):
     dashboard_page = DashboardPage(driver)
     # Find statuses on page before new status creation
     old_status_list = dashboard_page.statuses
