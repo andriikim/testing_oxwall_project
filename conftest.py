6d2cbbeab0ac38b3cec261d530bb6c3741ad9cbc
Olena Piankova <el.piankova@gmail.com>
2019-05-16 19:25:41 +0300

    DB fixture added: create user before test, delete it after test

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@ import json
 import os
 import pytest
 from selenium import webdriver
+
+from db_connector import OxwallDB
 from pages.pages import OxwallApp
 from models import User
 
@@ -21,12 +23,21 @@ def app(driver):
     driver.get(base_url)
     return OxwallApp(driver)
 
+@pytest.fixture(scope="session")
+def db():
+    db = OxwallDB()
+    yield db
+    db.close()
+
 with open(os.path.join(PROJECT_DIR, "data", "user_data.json"), encoding="utf8") as f:
     user_data_list = json.load(f)
 
 @pytest.fixture(params=user_data_list, ids=[str(user) for user in user_data_list])
-def user(request):
-    return User(**request.param)
+def user(request, db):
+    user = User(**request.param)
+    db.create_user(user)
+    yield user
+    db.delete_user(user)
 
 @pytest.fixture()
 def logged_user(driver, app):
---
+++ db_connector.py
@@ -0,0 +1,53 @@
+import pymysql
+
+from models import User
+
+
+def _our_hash(password):
+    d = {
+        "pass": "bf6116af8e4b3e83a7646640590b9d5f5c95b06bf7eebf6c424487ff39293833",
+        "test": "62fc22c0da68a727562013a405e45ad29fe67725db24870d8dff48a39b37f5ae",
+        "secret": "94d1297b55907d7158b27cd91f0d0b0d212abc0ccd4a3e861b1f4e1f404c67e0"
+    }
+    return d[password]
+
+
+class OxwallDB:
+    def __init__(self):
+        self.connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='mysql',
+                             db='oxwa166',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+    def close(self):
+        self.connection.close()
+
+    def create_user(self, user):
+        with self.connection.cursor() as cursor:
+            # Create a new user
+            sql = f"""INSERT `ow_base_user` (`username`, `email`, `password`, `joinIp`)
+                      VALUES ("{user.username}", "{user.email}", "{_our_hash(user.password)}", "21423532")"""
+            cursor.execute(sql)
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        self.connection.commit()
+
+    def delete_user(self, user):
+        with self.connection.cursor() as cursor:
+            sql = f'''DELETE FROM `ow_base_user`
+                      WHERE `ow_base_user`.`username` = "{user.username}"'''
+            cursor.execute(sql)
+        self.connection.commit()
+
+    def get_users(self):
+        """
+        :return: User objects with user data from DB
+        """
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM `ow_base_user`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        self.connection.commit()
+        return [User(**u) for u in result]  # TODO explain!
