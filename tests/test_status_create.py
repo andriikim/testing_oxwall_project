acdf8a2a635a897a2247258c45f940dcc26564ac
Olena Piankova <el.piankova@gmail.com>
2019-05-25 17:56:47 +0300

    Allure report: first approach (maybe will be obsolete)

--- tests/test_status_create.py
+++ tests/test_status_create.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pytest
+import allure
 
 from conftest import PROJECT_DIR
 from data.random_string import random_string
@@ -13,18 +14,23 @@ with open(os.path.join(PROJECT_DIR, "data", "status_data.json"), encoding="utf8"
 status_text_list.append(random_string())
 # status_text_list.extend([random_string() for _ in range(5)])
 
+
+@allure.feature("Status feature")
+@allure.story("Create status")
 @pytest.mark.parametrize("input_text", status_text_list)
 def test_create_status(driver, logged_user, input_text, db):
     dashboard_page = DashboardPage(driver)
-    # Find statuses on page before new status creation
-    old_status_list = dashboard_page.statuses
-    assert dashboard_page.status_input_field.placeholder == "What’s happening?"
-    dashboard_page.create_new_text_status(input_text)
-    # Wait until new status appears
-    dashboard_page.wait_new_status_appear(old_status_list)
-    # Verify text of new status
-    assert db.get_last_text_status() == input_text
-    new_status = dashboard_page.statuses[0]
-    assert new_status.text == input_text
-    assert new_status.user == logged_user
-    assert new_status.time == "within 1 minute"
+    with allure.step("Given initial amount of status in Oxwall database"):
+        old_status_list = dashboard_page.statuses
+    with allure.step(f'When I add a status with "{input_text}" in Dashboard page'):
+        dashboard_page.create_new_text_status(input_text)
+        # Wait until new status appears
+        dashboard_page.wait_new_status_appear(old_status_list)
+    with allure.step("Then a new status block appears before old list of status"):
+        # Verify text of new status
+        assert db.get_last_text_status() == input_text
+        new_status = dashboard_page.statuses[0]
+    with allure.step(f"Then this status block has this {input_text} and author as this user {logged_user} and time \"within 1 minute\""):
+        assert new_status.text == input_text
+        assert new_status.user == logged_user
+        assert new_status.time == "within 1 minute"
